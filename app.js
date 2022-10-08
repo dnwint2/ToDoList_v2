542c9cf344aee1518660c65beae564881f559ffc
Daniel W <dnwint2@gmail.com>
2022-10-08 16:38:56 -0400

    Added error logging in mongoose.connect catch block

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ mongoose
   .then(function() {
     console.log("Successfully connected to Mongodb Atlas DB.");
   })
-  .catch(function() {
+  .catch(function(error) {
     console.log("Failed to connect to Mongodb Atlas DB.");
+    console.log(error);
   }); //?retryWrites=true&w=majority
 //mongodb+srv://dnwint2:<password>@cluster0.iegpich.mongodb.net/?retryWrites=true&w=majority
 
