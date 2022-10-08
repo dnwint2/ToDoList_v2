cfc25ef2e7b550de2570b342153a53e5526acb13
Daniel W <dnwint2@gmail.com>
2022-10-08 01:57:26 -0400

    Adjust cnxn string for mongodb Atlas.

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ app.use(express.static("public"));
 //connect to mongoose db (create if dne)
 mongoose
   .connect(
-    "mongodb+srv://dnwint2:66UQtkHUHEfJdr.@cluster0.iegpich.mongodb.net/todolistDB//?retryWrites=true@w=majority"
+    "mongodb+srv://dnwint2:66UQtkHUHEfJdr.@cluster0.iegpich.mongodb.net/?retryWrites=true@w=majority"
     // "mongodb://localhost:27017/todolistDB"
+    // 66UQtkHUHEfJdr.
+    // mongodb+srv://dnwint2:<password>@cluster0.iegpich.mongodb.net/?retryWrites=true&w=majority
   )
   .then(function() {
     console.log("Successfully connected to Mongodb Atlas DB.");
